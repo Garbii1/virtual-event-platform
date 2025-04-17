03ecef2653a1f0a1e681551120305f1c961482e4
Garbii1 <muhdgaruba@gmail.com>
2025-04-17 19:44:48 +0100

    Update production API URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'YOUR_RENDER_BACKEND_URL' // Replace with your deployed Render URL later
+    apiUrl: 'https://virtual-event-platform-kjio.onrender.com' // Replace with your deployed Render URL later
   };
